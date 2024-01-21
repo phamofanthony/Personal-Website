7e92929c837d6f0f94a6d9e756f2921bb3d46049
phamofanthony <80490069+phamofanthony@users.noreply.github.com>
2024-01-21 04:48:15 -0600

    updated build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {Link, useLocation} from 'react-router-dom';
 import "../styles/Navbar.css";
-import ReorderIcon from '@material-ui/icons/Reorder';
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
     const [expandNavbar, setExpandNavbar] = useState(false);
