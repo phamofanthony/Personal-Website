38b699bd3437361e8418a24a54616d891432821d
phamofanthony <80490069+phamofanthony@users.noreply.github.com>
2024-01-21 17:34:30 -0600

    mobile fix

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   }, []);
   return (
     <div className="App">
+      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       <Router>
         <Navbar />
         <Routes>
