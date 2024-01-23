30f1703906704e7a8c7447d47e47efbee040208f
phamofanthony <80490069+phamofanthony@users.noreply.github.com>
2024-01-23 10:11:42 -0600

    Github

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -5,45 +5,54 @@ export const ProjectList = [
     name: "Personal Website",
     image: Proj1,
     skills: "ReactJS, HTML/CSS, NPM, Yarn, Node.JS, Webapp Deployment",
+    github: "https://github.com/phamofanthony/Personal-Website",
   },
   {
     name: "VSA Planning Website",
     image: Proj1,
     skills: "Python, Django, HTML/CSS, Google API",
+    github: "",
   },
   {
     name: "Nuqleous Status Slack Bot",
     image: Proj1,
     skills: "Python, Slack API, Airflow API, PostgreSQL",
+    github: "",
   },
   {
     name: "Capstone Matching Web Application",
     image: Proj1,
     skills: "Python, Django, HTML/CSS, OR-Tools",
+    github: "",
   },
   {
     name: "Nuqleous Automated Customer Setup",
     image: Proj1,
     skills: "C#/.NET, Blazor, HTML/CSS, MicroStrategy API, internal API's",
+    github: "",
   },
   {
     name: "AI Pacman Solver",
     image: Proj1,
     skills: "Python, Pygame",
+    github: "",
   },
   {
     name: "Food Map Mobile Application",
     image: Proj1,
-    skills: "Kotlin, Android development, XAML, Firebase FireStore, NoSQL"
+    skills: "Kotlin, Android development, XAML, Firebase FireStore, NoSQL",
+    github: "https://github.com/phamofanthony/Food-Map",
   },
   {
     name: "UARK Campus Navigation Program",
     image: Proj1,
     skills: "C++, Searching algorithms, Sorting algorithms, Optimization",
+    github: "",
   },
   {
     name: "Bridge Crossing Simulation",
     image: Proj1,
-    skills: "C, Pthreads, Multithreaded development, Unix"
+    skills: "C, Pthreads, Multithreaded development, Unix",
+    github: "",
   },
 ];
