79cf902991de9bf00729862b937ef1c7cdb6e287
phamofanthony <80490069+phamofanthony@users.noreply.github.com>
2024-01-25 13:46:49 -0600

    Mostly finished website. Unpolished.

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -1,28 +1,38 @@
-import React from 'react'
-import {useParams} from 'react-router-dom'
-import {ProjectList} from '../helpers/ProjectList'
-import GitHubIcon from '@material-ui/icons/GitHub'
-import "../styles/ProjectDisplay.css"
+import React from "react";
+import { useParams } from "react-router-dom";
+import { ProjectList } from "../helpers/ProjectList";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import "../styles/ProjectDisplay.css";
 
 function ProjectDisplay() {
-    const {id} = useParams()
-    const project = ProjectList[id]
-    return (
-      <div className="project">
-        <h1>{project.name}</h1>
-        <img src={project.image} alt="Visual of my project." />
-        <p>{project.desc}</p>
-        <p>
-          <b>Skills:</b> {project.skills} <br />
-        </p>
+  const { id } = useParams();
+  const project = ProjectList[id];
 
-        {project.github && (
-          <a href={project.github} target="_blank" rel="noopener noreferrer">
-            <GitHubIcon />
-          </a>
+  return (
+    <div className="project">
+      <h1>{project.name}</h1>
+      <div className="media">
+        {project.mediaType === "image" ? (
+          <img src={project.media} alt="Visual of my project." />
+        ) : (
+          <video autoPlay muted controls>
+            <source src={project.media} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         )}
       </div>
-    );
+      <p>{project.desc}</p>
+      <p>
+        <b>Skills:</b> {project.skills} <br />
+      </p>
+
+      {project.github && (
+        <a href={project.github} target="_blank" rel="noopener noreferrer">
+          <GitHubIcon />
+        </a>
+      )}
+    </div>
+  );
 }
 
-export default ProjectDisplay
+export default ProjectDisplay;
